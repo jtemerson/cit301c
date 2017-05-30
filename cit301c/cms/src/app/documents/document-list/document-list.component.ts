0a52280a99f26b525afc367b3013aa9003552268
jtemerson <eme13001@byui.edu>
2017-05-30 14:33:27 -0600

    Lesson 4 completed

--- cit301c/cms/src/app/documents/document-list/document-list.component.ts
+++ cit301c/cms/src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+import { Document } from '../document.model';
 
 @Component({
   selector: 'cms-document-list',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./document-list.component.css']
 })
 export class DocumentListComponent implements OnInit {
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+
+  document: Document[] = [
+    new Document(1, 'Bro. Jackson', 'Description', 'https://web.byui.edu/Directory/Employee/jacksonk.jpg', 'children'),
+    new Document(1, 'Bro. Jackson', 'Description', 'https://web.byui.edu/Directory/Employee/jacksonk.jpg', 'children'),
+    new Document(1, 'Bro. Jackson', 'Description', 'https://web.byui.edu/Directory/Employee/jacksonk.jpg', 'children'),
+    new Document(1, 'Bro. Jackson', 'Description', 'https://web.byui.edu/Directory/Employee/jacksonk.jpg', 'children'),
+  ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelectedDocument(document: Document){
+    this.selectedDocumentEvent.emit(document);
   }
 
 }
