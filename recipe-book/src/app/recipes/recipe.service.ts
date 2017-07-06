a12c0bd47b2068033625702b4791f5d424f346d3
jtemerson <eme13001@byui.edu>
2017-07-05 23:42:49 -0600

    lesson 7 recipe book

--- recipe-book/src/app/recipes/recipe.service.ts
+++ recipe-book/src/app/recipes/recipe.service.ts
@@ -1,28 +1,28 @@
-import { EventEmitter, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredients.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
-
+import { Subject } from 'rxjs/Subject';
 
 @Injectable()
 
 export class RecipeService{
-  recipeSelected = new EventEmitter<Recipe>();
+  recipesChanged = new Subject<Recipe[]>();
 
   private recipes: Recipe[] = [
     new Recipe(
-      'test'
-      , 'this is a test'
-      , 'http://assets-jpcust.jwpsrv.com/thumbs/Klop5Noi-720.jpg'
-      ,[
-        new Ingredient('meat', 1)
-        ,new Ingredient('french fries', 20)
+      'test',
+      'this is a test',
+      'http://assets-jpcust.jwpsrv.com/thumbs/Klop5Noi-720.jpg',
+      [
+        new Ingredient('meat', 1),
+        new Ingredient('french fries', 20)
       ]),
     new Recipe(
-      'another test'
-      , 'this is a test'
-      , 'http://assets-jpcust.jwpsrv.com/thumbs/Klop5Noi-720.jpg'
-      ,[
+      'another test',
+      'this is a test',
+      'http://assets-jpcust.jwpsrv.com/thumbs/Klop5Noi-720.jpg',
+      [
         new Ingredient('buns', 2)
         ,new Ingredient('meat', 1)
       ]),
@@ -42,5 +42,20 @@ export class RecipeService{
 
   addIngredientsToShoppingList(ingredients: Ingredient[]){
     this.slService.addIngredients(ingredients);
+  }
+
+  addRecipe(recipe: Recipe){
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe){
+    this.recipes[index] = newRecipe
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  deleteRecipe(index: number){
+    this.recipes.splice(index, 1)
+    this.recipesChanged.next(this.recipes.slice())
   }
 }
